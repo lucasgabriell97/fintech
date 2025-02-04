73275843c2eab3937d9ddcea8cf802e6fbeaeb08
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2025-02-03 21:19:05 -0300

    feat: adiciona listagem do status das vendas

--- src/components/Months/Months.tsx
+++ src/components/Months/Months.tsx
@@ -4,6 +4,7 @@ import { MonthBtn } from "../MonthBtn/MonthBtn";
 export const Months: React.FC = () => {
   return (
     <div className="flex">
+      <MonthBtn n={-3} />
       <MonthBtn n={-2} />
       <MonthBtn n={-1} />
       <MonthBtn n={0} />
